9bced93a5a052a1ed992f1468f41cc8236083414
Eugene Lysov <evg.lysov@gmail.com>
2021-03-14 15:59:48 +0200

    Updates

---
+++ precache-manifest.bc44aa6a42d85453c8665e4135fd31ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b79f3477b18020dcc70c",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.cbb68cdb.chunk.css"
+  },
+  {
+    "revision": "b79f3477b18020dcc70c",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.7a0d70b4.chunk.js"
+  },
+  {
+    "revision": "d586fd842c3e6fc2d4e4",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime~main.b0c9dd7b.js"
+  },
+  {
+    "revision": "f8b0074219fea6a0169c",
+    "url": "/goit-react-hw-07-phonebook/static/css/2.5919c94a.chunk.css"
+  },
+  {
+    "revision": "f8b0074219fea6a0169c",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.75a42ea2.chunk.js"
+  },
+  {
+    "revision": "7eaf2e1067ad32b984b6bd43aacf90ee",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  }
+];
